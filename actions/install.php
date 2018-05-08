3ad2bd5d66cab9f23a96262f1e43723dae19c39c
Carlos Santos <c42759@users.noreply.github.com>
2018-05-08 12:44:56 +0100

    bug fix, wrong name of class

--- actions/install.php
+++ actions/install.php
@@ -1,45 +1,33 @@
 <?php
 
 if (isset($_POST["submitInstall"]) && user::isOwner($authData)) {
-	$query = bo3::c2r(
-		[
-			'mod-name' => $cfg->mdl->name,
-			'mod-folder' => $cfg->mdl->folder,
-			'prefix' => $cfg->db->prefix
-		],
-		bo3::mdl_load("db/install.sql")
-	);
+	$query = bo3::c2r([
+		'mod-name' => $cfg->mdl->name,
+		'mod-folder' => $cfg->mdl->folder,
+		'prefix' => $cfg->db->prefix
+	], bo3::mdl_load("db/install.sql"));
 
 	if ($db->multi_query($query) != FALSE) {
 		while ($db->more_results() && $db->next_result()) {;} // flush multi_queries
 
-		copy("modules/{$cfg->mdl->folder}/install/class.emailqueue.php", "class/class.emailqueue.php");
+		copy("modules/{$cfg->mdl->folder}/install/class.1-emailqueue.php", "class/class.1-emailqueue.php");
 		copy("modules/{$cfg->mdl->folder}/install/mod-emailqueue-cron.php", "cron/mod-emailqueue-cron.php");
 		copy("modules/{$cfg->mdl->folder}/install/mod-emailqueue-api.php", "../pages/mod-emailqueue-api.php");
 
-		$mdl = bo3::c2r(
-			[
-				'lg-message' => $lang["install"]["success"]
-			],
-			bo3::mdl_load("templates-e/install/message.tpl")
-		);
+		$mdl = bo3::c2r([
+			'lg-message' => $lang["install"]["success"]
+		], bo3::mdl_load("templates-e/install/message.tpl"));
 	} else {
-		$mdl = bo3::c2r(
-			[
-				'lg-message' => $lang["install"]["failure"]." : ".$db->error,
-			],
-			bo3::mdl_load("templates-e/install/message.tpl")
-		);
+		$mdl = bo3::c2r([
+			'lg-message' => $lang["install"]["failure"]." : ".$db->error,
+		], bo3::mdl_load("templates-e/install/message.tpl"));
 	}
 } else {
-	$mdl = bo3::c2r(
-		[
-			'lg-install' =>$lang["install"]["question"],
-			'lg-yes' => $lang["common"]["a-yes"],
-			'lg-no' => $lang["common"]["a-no"]
-		],
-		bo3::mdl_load("templates-e/install/form.tpl")
-	);
+	$mdl = bo3::c2r([
+		'lg-install' =>$lang["install"]["question"],
+		'lg-yes' => $lang["common"]["a-yes"],
+		'lg-no' => $lang["common"]["a-no"]
+	], bo3::mdl_load("templates-e/install/form.tpl"));
 }
 
 include "pages/module-core.php";
