85f583d7fc9244e7912a84c075d5a1a66f7a631f
Carlos Santos <carlos@one-shift.com>
2018-05-08 18:24:08 +0100

    solved problem with SSL/Https requests

--- install/mod-emailqueue-cron.php
+++ install/mod-emailqueue-cron.php
@@ -1,7 +1,10 @@
 <?php
+
 include dirname(__FILE__)."/../config/cfg.php";
 include dirname(__FILE__)."/../config/system.php";
 
 include dirname(__FILE__)."/../class/class.bo3.php";
 
-$result = file_get_contents("{$cfg->system->protocol}://{$cfg->system->domain}{$cfg->system->path}/en/mod-emailqueue-api/");
+$options = ["ssl" => [ "verify_peer" => false, "verify_peer_name" => false ]];
+
+$result = file_get_contents("{$cfg->system->protocol}://{$cfg->system->domain}{$cfg->system->path}/en/mod-emailqueue-api/", false, stream_context_create($options));
