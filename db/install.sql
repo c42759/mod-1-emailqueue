3f145579af3555f0b22190ae5113a6217eaf0dd5
Rafael Duarte <rafaeljfduarte@gmail.com>
2018-05-28 17:18:05 +0100

    Install Fix

--- db/install.sql
+++ db/install.sql
@@ -1,26 +1,35 @@
-INSERT INTO `{c2r-prefix}_modules` (`name`, `folder`, `code`, `sort`) VALUES ("{c2r-mod-name}", "{c2r-mod-folder}", "{c2r-mod-code}", 0);
+INSERT INTO `{c2r-prefix}_modules` (`name`, `folder`, `code`, `sort`) VALUES ("{c2r-mod-name}", "{c2r-mod-folder}", "{'fa-icon':'fa-envelope','img':'', 'sub-items':{}}", 0);
 
-CREATE TABLE `{c2r-prefix}_email_queue_settings` (
+CREATE TABLE `{c2r-prefix}_1_email_queue_settings` (
 	`id` int(11) NOT NULL,
 	`name` varchar(255) NOT NULL,
 	`value` text NOT NULL,
 	`date` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-	`date_update` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
+	`date_update` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+ALTER TABLE `{c2r-prefix}_1_email_queue_settings`
+	ADD PRIMARY KEY (`id`);
 
-INSERT INTO `{c2r-prefix}_email_queue_settings` (`name`, `value`) VALUES
+ALTER TABLE `{c2r-prefix}_1_email_queue_settings`
+	MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+INSERT INTO `{c2r-prefix}_1_email_queue_settings` (`name`, `value`) VALUES
 ('delay', '300'),
 ('error_times_limit', '5'),
+('server_email_name', 'One:Shift'),
 ('server_email', 'hello@one-shift.com'),
+('reply_to', 'hello@one-shift.com'),
 ('server_smtp', 'mail.yourdomain.here'),
 ('server_username', 'your@email.here'),
 ('server_password', 'yourpassword'),
 ('server_port', 'connection-port eg.: 25'),
 ('server_secure', 'connection-type eg.: plain'),
-('server_debug', '1');
+('server_debug', '1'),
+('default_subject', 'Write here Default Subject'),
+('default_content', 'Write here Default Content, use HTML if you want');
 
-CREATE TABLE `{c2r-prefix}_email_queue` (
+CREATE TABLE `{c2r-prefix}_1_email_queue` (
 	`id` int(11) NOT NULL,
 	`from` varchar(255) NOT NULL,
 	`to` varchar(255) NOT NULL,
@@ -30,6 +39,13 @@ CREATE TABLE `{c2r-prefix}_email_queue` (
 	`content` text NOT NULL,
 	`attachments` text NOT NULL,
 	`status` tinyint(1) NOT NULL DEFAULT '0',
+	`priority` int(11) NOT NULL,
 	`date` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-	`date_update` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
+	`date_update` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+ALTER TABLE `{c2r-prefix}_1_email_queue`
+	ADD PRIMARY KEY (`id`);
+
+ALTER TABLE `{c2r-prefix}_1_email_queue`
+	MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
