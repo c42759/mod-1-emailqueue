fbb2ed3ff1dfaac828deb6d49b1877e982881701
Carlos Santos <c42759@users.noreply.github.com>
2018-06-04 12:16:15 +0100

    Update mod-1-emailqueue-cron.php

--- install/mod-1-emailqueue-cron.php
+++ install/mod-1-emailqueue-cron.php
@@ -7,4 +7,4 @@ include dirname(__FILE__)."/../class/class.bo3.php";
 
 $options = ["ssl" => [ "verify_peer" => false, "verify_peer_name" => false ]];
 
-$result = file_get_contents("{$cfg->system->protocol}://{$cfg->system->domain}{$cfg->system->path}/en/mod-emailqueue-api/", false, stream_context_create($options));
+$result = file_get_contents("{$cfg->system->protocol}://{$cfg->system->domain}{$cfg->system->path}/en/mod-1-emailqueue-api/", false, stream_context_create($options));
